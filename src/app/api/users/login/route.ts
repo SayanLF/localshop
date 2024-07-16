ddae7e890bae970d5ae0566a41ac0ea202743a16
Subham <subhamkumarsadhukhan@gmail.com>
2024-07-16 11:46:27 +0530

    fix login

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -10,20 +10,25 @@ export async function POST(request : NextRequest) {
     try {
         const reqBody = await request.json()
         const {email, password} = reqBody;
+        console.log(reqBody)
         const user = await User.findOne({email})
+        console.log(user);
         if(!user){
             return NextResponse.json({error: "User Does not exist"}, {status: 400})
         }
         const validPassword = await bcryptjs.compare(password, user.password)
+        console.log(validPassword);
         if(!validPassword){
-            return NextResponse.json({error: "Invalid Password"}, {status: 400})
+            return NextResponse.json({error: "Invalid Password"}, {status: 300})
         }
         const tokenData = {
             id: user._id,
             username: user.username,
             email: user.email
         }
+        console.log(tokenData,Object.keys(jwt),process.env.TOKEN_SECRET)
         const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {expiresIn: "1d"})
+        console.log(token)
         const response = NextResponse.json({
             message: "Login Successful",
             success: true,
@@ -35,5 +40,6 @@ export async function POST(request : NextRequest) {
 
     } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
+        
     }
 }
